2670a4b5fa2e074d8177e36a3d8b019e3b00f4c3
Jooker302 <i.omer6637@gmail.com>
2023-01-15 00:20:49 +0500

    Back Button Lcok

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -11,6 +11,10 @@ import java.util.List;
 
 public class Home extends AppCompatActivity {
 
+    private static final int TIME_DELAY = 2000;
+    private static long back_pressed;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,20 @@ public class Home extends AppCompatActivity {
         Intent i = new Intent(this, CreatePost.class);
         startActivity(i);
     }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+//        finishAffinity();
+        if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+            finishAffinity();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        back_pressed = System.currentTimeMillis();
+    }
+//    }
+
 
     public void viewpost(View v){
 //        Toast.makeText(this,"allposts".toString(),Toast.LENGTH_LONG).show();
